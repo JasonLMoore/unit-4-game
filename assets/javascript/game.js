da96caecb15e81a13e229871360c8b2d2075f2a4
Jason Moore <jason.moore1369@gmail.com>
2019-03-15 20:54:54 -0500

    random number generator

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+
+var crystalValue = 0;
+var totalWins = 0;
+var totalLosses = 0;
+
+
+
+var makeTarget = function() {
+    var targetMin = 19;
+    var targetMax = 120;
+    var targetArray = [];
+    for (var i = targetMin; i < targetMax+1; i++) {
+        targetArray.push(i);
+    };
+    console.log(targetArray);
+    var randomNumber = targetArray[Math.floor(Math.random() * targetArray.length)];
+    console.log(randomNumber);
+};
+
+console.log(makeTarget());
+    
+    
+
